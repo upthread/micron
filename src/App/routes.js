841b2e3043bf99bac387452fcc0381aa388b2289
tactnician <tactnician@gmail.com>
2019-06-19 09:43:10 -0400

    updated routes and added to Nav

--- src/App/routes.js
+++ src/App/routes.js
@@ -4,13 +4,14 @@ import { Route } from 'react-router-dom'
 import Form from '../Form'
 import Template from '../Template'
 import MultiStepForm from '../MultiStepForm'
+import Splash from '../Splash'
 
 const routes = () => {
     return (
         <>
-            <Route path='/form'  component={Form} /> 
-            <Route path='/template'  component={Template} /> 
-            <Route path='/multistep'  component={MultiStepForm} /> 
+            <Route exact path='/'  component={Splash} /> 
+            <Route path='/templ'  component={Template} /> 
+            <Route path='/multi'  component={MultiStepForm} /> 
             {/* <Route path='/'  component={} />  */}
             {/* <Route path='/'  component={} />  */}
             {/* <Route path='/'  component={} />  */}
--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <div>
-            
+        <div className='nav_container'>
+            <header>
+                <h3 className= "logo"><Link to= "/home">RMT</Link></h3>
+                <a className="to_nav" href='#primary_nav'>Menu</a>
+            </header>
+            <nav id= 'primary_nav'>
+                <Link to= '/'>Home</Link>
+                <Link to= '/templ'>Form</Link>
+                <Link to= '/multi'>Multi</Link>
+                {/* <Link to= '/'></Link> */}
+                {/* <Link to= '/'></Link> */}
+                {/* <Link to= '/'></Link> */}
+            </nav>
         </div>
     )
 }
