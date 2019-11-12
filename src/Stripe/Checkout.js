87138b88a98ec923030b326543b14d4d4149392e
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-12 11:09:18 -0500

    Added function to hit the backend end point with data

--- src/Stripe/Checkout.js
+++ src/Stripe/Checkout.js
@@ -1,23 +1,21 @@
 import React from 'react';
-
+import PouchDB from 'pouchdb';
+import axios from 'axios';
 import './App.css';
 import StripeCheckout from "react-stripe-checkout";
-
-import PouchDB from 'pouchdb';
 
 var tokens = new PouchDB('tokens');
 
 function Checkout() {
 
   const [product] = React.useState({
-    name:"California",
+    name:"TestProduct",
     price: 20,
     description:"Silicon Valley"
   });
 
-
-   //this function parses the relavent information needed for the backend and puts the "JSON blob" into Pouchdb to be synced and authenticated  with the backend later
-   const convert = (token)=>{
+  //this function parses the relavent information needed for the backend and puts the "JSON blob" into Pouchdb to be synced and authenticated  with the backend later
+  const convert = (token)=>{
     
     let charge ={
       _id: token.id,
@@ -37,13 +35,32 @@ function Checkout() {
         }
       }
     }
-    console.log(charge);
+    console.log("what will be in the DB",charge);
     tokens.put(charge)
   }
 
-  const handleToken = (token, addresses)=>{
-    convert(token);
+  //turning this into async to get the value of post request
+  async function handleBackend(token,product){
+    console.log("async ran")
+    //awaits the resolution of this request and puts the response into a variable
+   const response = await axios.post("http://localhost:8080/checkout", {
+      token,
+      product
+    });
+    const {status} = response.data;
+
+    alert("from backend", status)
+    console.log(status)
   }
+
+
+//token and billing and shipping addresses provided
+  const handleToken = (token, addresses)=>{
+    // convert(token);
+    handleBackend(token, product);
+  }
+
+  
 
   return (
     
@@ -53,7 +70,7 @@ function Checkout() {
 
       {/* pk_test_nOBc87GRC4nCiUc6w8u5NNF800dQqGqVgn */}
       {/* sk_test_w7tGMY2X9EanZkcYrVc1K8Oq00ThL3d4UJ */}
-        Yeet
+        
         <div className="product">
           <h1>{product.name}</h1>
           <h2>Price: {(product.price)*100}</h2>
@@ -66,7 +83,7 @@ function Checkout() {
         token={handleToken}
         billingAddress
         shippingAddress
-        amount={product.price}
+        amount={product.price * 100}
         name={product.name}
         
         />
